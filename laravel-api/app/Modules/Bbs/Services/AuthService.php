ab76df005a1c24e57f661357566dc112f228c134
小丑路人 <2278757482@qq.com>
2021-02-25 11:55:07 +0800

    API：登录与注册的Token，更换为 非对称加密解密方式

--- laravel-api/app/Modules/Bbs/Services/AuthService.php
+++ laravel-api/app/Modules/Bbs/Services/AuthService.php
@@ -2,9 +2,11 @@
 
 namespace App\Modules\Bbs\Services;
 
+use App\Constants\CacheKeys;
 use App\Exceptions\Bbs\AuthException;
 use App\Exceptions\Bbs\AuthTokenException;
 use App\Exceptions\Bbs\FailException;
+use App\Library\Encrypt\Rsa;
 use App\Models\Log\UserLoginLog;
 use App\Models\System\Notify;
 use App\Models\User\User;
@@ -91,9 +93,6 @@ class AuthService extends Service
 
             DB::commit();
 
-            $auth = Auth::guard($this->guard);
-            $auth->setUser($user);
-
             // 注册成功：站内系统消息发送
             Notify::insert([
                 'notify_type' => Notify::NOTIFY_TYPE['SYSTEM_MSG'],
@@ -112,7 +111,7 @@ class AuthService extends Service
             }
 
             $this->setError('注册成功，请完善个人资料！');
-            $result = $this->respondWithToken($auth->login($user));
+            $result = $this->respondWithToken($user->user_id);
             return array_merge($result, [
                 'user_avatar' => $user_info['user_avatar'],
                 'user_sex' => $user_info['user_sex'],
@@ -133,14 +132,13 @@ class AuthService extends Service
      */
     public function login($params): array
     {
-        $auth = Auth::guard($this->guard);
         $userInstance = User::getInstance();
 
         $auth_success = false;
         if ($user_name = $userInstance->getUserByName($params['user_name'])){
             if (hash_verify($params['password'], $user_name->password)){
                 $auth_success = true;
-                $auth->setUser($user_name);
+                $user = $user_name;
             }
         }
         if (
@@ -151,7 +149,7 @@ class AuthService extends Service
         {
             if (hash_verify($params['password'], $user_email->password)){
                 $auth_success = true;
-                $auth->setUser($user_email);
+                $user = $user_email;
             }
         }
         if (
@@ -162,14 +160,13 @@ class AuthService extends Service
         {
             if (hash_verify($params['password'], $user_mobile->password)){
                 $auth_success = true;
-                $auth->setUser($user_mobile);
+                $user = $user_mobile;
             }
         }
         // 如果账户、邮箱、手机号，都验证失败 ，那么登录失败
         if (!$auth_success){
             throw new AuthException('认证失败！');
         }
-        $user = $auth->user();
         if (!$user) throw new AuthException('账户不存在！');
         switch ($user->is_check) {
             case 0:
@@ -183,7 +180,7 @@ class AuthService extends Service
         // 登录日志
         UserLoginLog::getInstance()->add($user->user_id, 1, '登录成功');
 
-        return $this->respondWithToken($auth->login($user));
+        return $this->respondWithToken($user->user_id);
     }
 
     /**
@@ -222,29 +219,30 @@ class AuthService extends Service
     }
 
     /**
-     * Refresh a token.
-     * 刷新token，如果开启黑名单，以前的token便会失效。
-     * 值得注意的是用上面的getToken再获取一次Token并不算做刷新，两次获得的Token是并行的，即两个都可用。
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function refresh()
-    {
-        return $this->respondWithToken(auth($this->guard)->refresh());
-    }
-
-    /**
      * Get the token array structure.
      *
      * @param $token
      * @return array
      */
-    protected function respondWithToken($token): array
+    protected function respondWithToken($user_id): array
     {
+        $expire_time = time() + CacheKeys::KEY_DEFAULT_TIMEOUT;
+        $token = $this->getUserToken($user_id, $expire_time);
         return [
             'access_token' => $token,
-            'token_type'   => 'bearer',
-            'expires_time'   => time() + Auth::guard($this->guard)->factory()->getTTL() * 60
+            'expires_time'   => $expire_time,
         ];
+    }
+
+    /**
+     * 获取登录会员的Token
+     *
+     * @param  int  $user_id
+     *
+     * @return string|null
+     */
+    protected function getUserToken(int $user_id, int $expire_time)
+    {
+        return Rsa::publicEncrypt(['user_id' => $user_id, 'expire_time' => $expire_time]);
     }
 }
