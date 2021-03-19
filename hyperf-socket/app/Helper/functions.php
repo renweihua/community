5e80326d3e634b7278454f00dce41231a93e38c2
小丑路人 <2278757482@qq.com>
2021-03-19 15:30:36 +0800

    sync function

--- hyperf-socket/app/Helper/functions.php
+++ hyperf-socket/app/Helper/functions.php
@@ -1,5 +1,51 @@
 <?php
 
+
+if ( !function_exists('get_distance') ) {
+    /**
+     * 根据经纬度算距离，返回结果单位是公里，先纬度，后经度
+     *
+     * @param $lat1
+     * @param $lng1
+     * @param $lat2
+     * @param $lng2
+     * @return float|int
+     */
+    function get_distance($lat1, $lng1, $lat2, $lng2)
+    {
+        $EARTH_RADIUS = 6378.137;
+
+        $radLat1 = rad($lat1);
+        $radLat2 = rad($lat2);
+        $a = $radLat1 - $radLat2;
+        $b = rad($lng1) - rad($lng2);
+        $s = 2 * asin(sqrt(pow(sin($a / 2), 2) + cos($radLat1) * cos($radLat2) * pow(sin($b / 2), 2)));
+        $s = $s * $EARTH_RADIUS;
+        $s = round($s * 10000) / 10000;
+
+        return $s;
+    }
+
+    function rad($d)
+    {
+        return $d * M_PI / 180.0;
+    }
+}
+
+if ( !function_exists('get_encryption_idcard') ) {
+    /**
+     * 身份证号，文本内容部分加密
+     *
+     * @param  string  $identity_card
+     *
+     * @return string
+     */
+    function get_encryption_idcard(string $identity_card): string
+    {
+        return substr_replace($identity_card, '****', -9, 5);
+    }
+}
+
 if ( !function_exists('get_days_in_year') ) {
     /**
      * 获取指定年份有多少天
