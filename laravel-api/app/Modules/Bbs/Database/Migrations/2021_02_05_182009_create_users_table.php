e1dd0a4e7f7e8d08c20be0028414830a7d27e7c2
小丑路人 <2278757482@qq.com>
2021-07-01 17:11:19 +0800

    update

--- laravel-api/app/Modules/Bbs/Database/Migrations/2021_02_05_182009_create_users_table.php
+++ laravel-api/app/Modules/Bbs/Database/Migrations/2021_02_05_182009_create_users_table.php
@@ -21,7 +21,6 @@ class CreateUsersTable extends Migration
             $table->string('user_name', 200)->default('')->comment('用户名');
             $table->string('user_email', 200)->default('')->comment('邮箱');
             $table->string('password', 60)->default('')->comment('登录密码');
-            $table->string('login_token', 100)->default('')->comment('login_token【用于提示是否异地登录，jwt-token只能检测是否登录---这个效果其实用不用都可以的，需要看心情。】');
             $table->boolean('is_check')->unsigned()->default(1)->comment('是否审核：1：正常；0：禁用；2.踢出登录，重新登录');
             $table->index(['is_check']);
         });
