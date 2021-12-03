0ec0cfaedb0cd2e5295bf77ebcf5edff1dc681a1
小丑路人 <2278757482@qq.com>
2021-12-03 19:43:14 +0800

    getIsDelete 默认为强制删除

--- laravel-api/app/Models/SoftDelete/SoftDelete.php
+++ laravel-api/app/Models/SoftDelete/SoftDelete.php
@@ -10,7 +10,7 @@ trait SoftDelete
 {
     public function getIsDelete()
     {
-        return $this->is_delete;
+        return $this->is_delete ?? 1;
     }
 
     public function getDeleteField()
@@ -77,7 +77,7 @@ trait SoftDelete
 
         $this->{$this->getDeletedAtColumn()} = $time;
 
-        if ($this->timestamps && ! is_null($this->getUpdatedAtColumn())) {
+        if ($this->timestamps && $this->getUpdatedAtColumn() == 0) {
             $this->{$this->getUpdatedAtColumn()} = $time;
 
             $columns[$this->getUpdatedAtColumn()] = $this->fromDateTime($time);
@@ -104,7 +104,7 @@ trait SoftDelete
             return false;
         }
 
-        $this->{$this->getDeletedAtColumn()} = null;
+        $this->{$this->getDeletedAtColumn()} = 0;
 
         // Once we have saved the model, we will fire the "restored" event so this
         // developer will do anything they need to after a restore operation is
@@ -125,7 +125,7 @@ trait SoftDelete
      */
     public function trashed()
     {
-        return ! is_null($this->{$this->getDeletedAtColumn()});
+        return $this->{$this->getDeletedAtColumn()} != 1;
     }
 
     /**
