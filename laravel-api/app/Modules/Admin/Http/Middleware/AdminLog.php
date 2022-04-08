b02913666bbc2de1fe8ca1ae4c23df33188df036
小丑路人 <2278757482@qq.com>
2022-04-08 17:16:03 +0800

    optimize

--- laravel-api/app/Modules/Admin/Http/Middleware/AdminLog.php
+++ laravel-api/app/Modules/Admin/Http/Middleware/AdminLog.php
@@ -25,10 +25,9 @@ class AdminLog
         $method = strtoupper($request->getMethod());
 
         if ($method != 'GET'){
-            $adminlog = \App\Models\Log\AdminLog::addRecord(!empty(auth($guard)->user()) ? auth($guard)->user()->admin_id : 0);
+            $admin_log = \App\Models\Log\AdminLog::addRecord(!empty(auth($guard)->user()) ? auth($guard)->user()->admin_id : 0);
 
             $log_status = 0;
-            $log_description = $adminlog->log_description;
             try{
                 $response = $next($request);
 
@@ -37,14 +36,14 @@ class AdminLog
 
                 // 根据接口响应，存储返回状态与文本提示语
                 $log_status = $response_body_content->status;
-                $log_description = $response_body_content->msg;
+                $log_description = empty($adminlog->log_description) ? $response_body_content->msg : $adminlog->log_description;
             }catch(\Exception $e){
                 $log_description = $e->getMessage();
                 $response = $this->errorJson($log_description);
             }
 
             // 同步更新响应状态与文本，在`handler`层可能会被异常终止
-            $adminlog->update(
+            $admin_log->update(
                 [
                     'log_duration' => microtime(true) - LARAVEL_START,
                     // 根据接口响应，存储返回状态与文本提示语
